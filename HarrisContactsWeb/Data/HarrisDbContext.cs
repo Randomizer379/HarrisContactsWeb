0fdbf3f2aa3debedc671f9a35ca6383612231988
Robert Walker <71553843+Randomizer379@users.noreply.github.com>
2020-12-13 23:12:44 +0000

    Add project files.

---
+++ HarrisContactsWeb/Data/HarrisDbContext.cs
@@ -0,0 +1,18 @@
+﻿using HarrisContactsWeb.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HarrisContactsWeb.Data
+{
+    public class HarrisDbContext : DbContext 
+    {
+        public HarrisDbContext(DbContextOptions<HarrisDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Contact> Contacts { get; set; }
+
+    }
+}
---
+++ HarrisContactsWeb/Models/Contact.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HarrisContactsWeb.Models
+{
+    public class Contact
+    {
+        public int ContactID { get; set; }
+        public int Name { get; set; }
+        public int Surname { get; set; }
+        public int Email { get; set; }
+        public int Phone { get; set; }
+        public int Address { get; set; }
+        public int City { get; set; }
+        public int Postcode { get; set; }
+        public int Type { get; set; }
+
+    }
+}
